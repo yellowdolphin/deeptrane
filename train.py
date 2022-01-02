d38ba89f7784e7499f2b44cd5ecbc84e72dfe169
yellowdolphin <marius.wanko@gmail.com>
2022-01-02 20:26:45 +0100

    kaggle support

--- train.py
+++ train.py
@@ -3,7 +3,8 @@ from subprocess import run
 from config import cfg
 
 # Install torch.xla on kaggle TPU supported nodes
-if os.path.exists('/opt/conda/lib/python3.7/site-packages/cloud_tpu_client'):
+if (os.path.exists('/opt/conda/lib/python3.7/site-packages/cloud_tpu_client') and not 
+    os.path.exists('/opt/conda/lib/python3.7/site-packages/cloud_tpu_profiler')):
     cfg.xla = True        # pull out of cfg?
     xla_version = '1.8.1' # only '1.8.1' works on python3.7
 
@@ -12,13 +13,14 @@ if os.path.exists('/opt/conda/lib/python3.7/site-packages/cloud_tpu_client'):
         capture_output=True)
     run(['python', 'pytorch-xla-env-setup.py', '--version', xla_version], capture_output=True)
     run('pip install -Uq --progress-bar off catalyst'.split(), capture_output=True)  # for DistributedSamplerWrapper
+    print("[ √ ] XLA:", xla_version)
 
 # Install timm
 if cfg.use_timm:
     try:
         import timm
     except ModuleNotFoundError:
-        run('pip install -q timm'.split(), capture_output=True)
+        run('pip install timm'.split(), capture_output=True)
         import timm
     print("[ √ ] timm:", timm.__version__)
 
