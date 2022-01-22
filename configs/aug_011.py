8060b8446db668b7ea1392e83715121b302e03b5
yellowdolphin <marius.wanko@gmail.com>
2022-01-22 22:33:12 +0100

    new configs

---
+++ configs/aug_011.py
@@ -0,0 +1,16 @@
+from torchvision.transforms.functional import InterpolationMode
+
+cfg = dict(
+    max_rotate         = 22.5,
+    jitter_brightness  = 0.2,
+    jitter_contrast    = 0.2,
+    hist_equalize      = 0.3,
+    p_cutout           = 0.75,
+    p_perspective      = 0,
+    interpolation      = InterpolationMode('bilinear'),
+)
+# Interpolation depends on library
+#                   nearest  lanczos  bilinear  bicubic  box/area  hamming
+# PIL                  0        1         2        3        4         5
+# albumentations       0                  1        3
+# cv2                  0        4         1        2        3
