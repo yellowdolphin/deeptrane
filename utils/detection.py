2a3a680c2b4ad28672746dda0eda6e0a1642dca1
yellowdolphin <marius.wanko@gmail.com>
2022-01-31 01:48:22 +0100

    fix utils.detection

--- utils/detection.py
+++ utils/detection.py
@@ -1,9 +1,12 @@
+import os
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import yaml
 
 from metadata import get_image_ids
+
 
 def get_bg_image_ids(df, bbox_col='boxes', id_col='image_id'):
     "Return array of image_ids with no bboxes"
@@ -47,11 +50,10 @@ def get_prediction_string(det_result, prec_b=4, prec_s=7, scale_x=1, scale_y=1):
     prediction_string = []
     for class_id, class_preds in enumerate(det_result):
         if len(class_preds) == 0: continue
-        s = ' '.join([
-            f'{class_id} {s:.{prec_s}} ' +
-            f'{x1 * scale_x:.{prec_b}f} {y1 * scale_y:.{prec_b}f} ' +
-            f'{x2 * scale_x:.{prec_b}f} {y2 * scale_y:.{prec_b}f}'
-            for x1, y1, x2, y2, s in class_preds])
+        s = ' '.join([(f'{class_id} {s:.{prec_s}} '
+                       f'{x1 * scale_x:.{prec_b}f} {y1 * scale_y:.{prec_b}f} '
+                       f'{x2 * scale_x:.{prec_b}f} {y2 * scale_y:.{prec_b}f}')
+                      for x1, y1, x2, y2, s in class_preds])
         prediction_string.append(s)
     return ' '.join(prediction_string) if prediction_string else '14 1 0 0 1 1'
 
@@ -69,13 +71,13 @@ def txt2prediction_string(filename, original_size=None, prec=4):
     "Convert YOLOv5 txt file into a VinBigData prediction string"
     H, W = original_size or (1, 1)
     preds = []
-    
+
     with open(filename, 'r') as fp:
         lines = fp.readlines()
-    
+
     if len(lines) == 0:
         return '14 1 0 0 1 1'   # code for no finding
-    
+
     for line in lines:
         class_id, x, y, w, h, score = line.split()
         x0, y0, x1, y1 = xywh2xyxy(x, y, w, h, W, H)
@@ -85,7 +87,7 @@ def txt2prediction_string(filename, original_size=None, prec=4):
 
 def txt2det_result(filename, n_classes, original_size=None, missing_ok=True):
     """Convert YOLOv5 txt file into iterable det_result[class_id] = class_preds
-    
+
     class_preds = [[x0, y0, x1, y1, score], ...]
     """
     H, W = original_size or (1, 1)
@@ -94,22 +96,23 @@ def txt2det_result(filename, n_classes, original_size=None, missing_ok=True):
     if not os.path.exists(filename) and missing_ok:
         det_result[14].append([0, 0, 1, 1, 1.0])  # code for no finding
         return det_result
-    
+
     with open(filename, 'r') as fp:
         lines = fp.readlines()
-    
+
     if len(lines) == 0:
         det_result[14].append([0, 0, 1, 1, 1.0])  # code for no finding
-        
+
     for line in lines:
         class_id, x, y, w, h, score = line.split()
         class_id = int(class_id)
         score = float(score)
-        x0, y0, x1, y1 = xywh2xyxy(x, y, w, h, W, H)        
+        x0, y0, x1, y1 = xywh2xyxy(x, y, w, h, W, H)
         class_pred = [x0, y0, x1, y1, score]
         det_result[class_id].append(class_pred)
-    
+
     return det_result
+
 
 def hub_results2prediction_string(results, prec=4):
     "Generate prediction_string from a single-image result (list of torch.tensor)"
@@ -117,6 +120,7 @@ def hub_results2prediction_string(results, prec=4):
     t = results.xyxyn[0]
     return ' '.join([f'{c:.0f} {s:.6f} {x0:.{prec}f} {y0:.{prec}f} {x1:.{prec}f} {y1:.{prec}f}'
                      for x0, y0, x1, y1, s, c in t])
+
 
 # --- My BBoxes class -----------------------------------------------------------------------
 class BBox():
@@ -128,68 +132,68 @@ class BBox():
             self.xyxy = np.array([float(s) for s in iterable.split()])
         else:
             self.xyxy = np.array(iterable)
-        
+
         if len(self.xyxy) != 4:
             msg = "BBox() takes iterable of len 4, but len is"
             raise ValueError(f"{msg} {len(self.xyxy)}")
         self.prec = prec or 0
-    
+
     def __str__(self):
         return ' '.join(f'{x:.{self.prec}f}' for x in self.xyxy)
-    
+
     def __repr__(self):
         prec_kwarg = '' if self.prec == 0 else f', prec={self.prec}'
         return f'BBox({list(self.xyxy)}{prec_kwarg})'
-    
+
     def __add__(self, b):
         if isinstance(b, self.__class__):
             return BBox(self.xyxy + b.xyxy)
         return BBox(self.xyxy + b, prec=self.prec)
-    
+
     def __sub__(self, b):
         if isinstance(b, self.__class__):
             return BBox(self.xyxy - b.xyxy)
         return BBox(self.xyxy - b, prec=self.prec)
-    
+
     def __radd__(self, b):
         return BBox(self.xyxy + b, prec=self.prec)
-    
+
     def __rsub__(self, b):
         return BBox(b - self.xyxy, prec=self.prec)
 
     def __iadd__(self, b):
         return BBox(self.xyxy + b, prec=self.prec)
-    
+
     def __isub__(self, b):
         return BBox(self.xyxy - b, prec=self.prec)
-    
+
     def __mul__(self, b):
         return BBox(self.xyxy * b, prec=self.prec)
-    
+
     def __truediv__(self, b):
         return BBox(self.xyxy / b, prec=self.prec)
-    
+
     def __itruediv__(self, b):
         return BBox(self.xyxy / b, prec=self.prec)
-        
+
     def __rmul__(self, b):
         return BBox(self.xyxy * b, prec=self.prec)
-    
+
     def __imul__(self, b):
         return BBox(self.xyxy * b, prec=self.prec)
-    
+
     def __eq__(self, b):
         if isinstance(b, self.__class__): return (self.xyxy == b.xyxy).all()
         return self == BBox(b)
-    
+
     def __neq__(self, b):
-        return not __eq__(self, b)
+        return not self.__eq__(self, b)
 
 
 class BBoxes(list):
     "List-like container for BBox instances with some array operators."
     def __init__(self, iterable=(), prec=None):
-        if isinstance(iterable, BBox): 
+        if isinstance(iterable, BBox):
             iterable = [iterable]
         len_is_4 = hasattr(iterable, '__len__') and (len(iterable) == 4)
         if len_is_4 and not any(isinstance(x, BBox) or hasattr(x, '__iter__') for x in iterable):
@@ -197,58 +201,58 @@ class BBoxes(list):
         iterable = [BBox(x, prec) for x in iterable]
         super().__init__(iterable)
         self.prec = prec
-    
+
     def __repr__(self):
         return f'BBoxes({[bbox for bbox in self]})'
-    
+
     def __str__(self):
         return f'{[str(bbox) for bbox in self]}'
-    
+
     def __add__(self, other):
         if isinstance(other, BBoxes):
             return BBoxes([a + b for a, b in zip(self, other)])
         return BBoxes([a + other for a in self])
-    
+
     def __sub__(self, other):
         if isinstance(other, BBoxes):
             return BBoxes([a - b for a, b in zip(self, other)])
         return BBoxes([a - other for a in self])
-    
+
     def __radd__(self, b):
         return BBoxes([a + b for a in self])
-    
+
     def __rsub__(self, b):
         return BBoxes([b - a for a in self])
 
     def __iadd__(self, b):
         return BBoxes([a + b for a in self])
-    
+
     def __isub__(self, b):
         return BBoxes([a - b for a in self])
-    
+
     def __mul__(self, b):
         return BBoxes([a * b for a in self])
-    
+
     def __truediv__(self, b):
         return BBoxes([a / b for a in self])
-    
+
     def __rmul__(self, b):
         return BBoxes([a * b for a in self])
-    
+
     def __imul__(self, b):
         return BBoxes([a * b for a in self])
-    
+
     def __itruediv__(self, b):
         return BBoxes([a / b for a in self])
-    
+
     @property
     def as_strings(self):
         "Return a list of str(BBox)"
         return [str(bbox) for bbox in self]
-    
+
     def annotation_string(self, class_ids, scores, thresh=0, score_prec=5):
         "Combine BBoxes, class_ids and scores in one str."
-        if isinstance(class_ids, int): 
+        if isinstance(class_ids, int):
             class_ids = [class_ids] * len(self)
         msg = f"len differ: {len(self)}, {len(class_ids)}, {len(scores)}"
         assert len(self) == len(class_ids) == len(scores), msg
@@ -259,11 +263,11 @@ class BBoxes(list):
             thresholds = thresh
         else:
             thresholds = [thresh] * len(self)
-        
+
         zipped = zip(class_ids, scores, self, thresholds)
         ann_strings = [f'{c} {s:.{score_prec}f} {b}' for c, s, b, t in zipped if s >= t]
         return ' '.join(ann_strings)
-        
+
     @classmethod
     def from_annotation_string(cls, s: str, class_id=None):
         "Create BBoxes from `s`, optionally filtering for `class_id`"
@@ -280,7 +284,7 @@ def split_siimcovid_boxes(df, cfg):
     labels = []
     for i, row in enumerate(df.itertuples()):
         for box in eval(row[cfg.bbox_col]):
-            image_df.append({
+            labels.append({
                 'image_id': row.image_id,
                 #'class_id': 0,
                 'fold': row.fold,
@@ -327,7 +331,7 @@ def write_dataset_yaml(cfg, path):
         ), fp, default_flow_style=False)
 
 
-def write_yolov5_labels(metadata, split, path)
+def write_yolov5_labels(metadata, split, path):
     for image_id in get_image_ids(metadata, split):
         d = metadata.loc[metadata.image_id == image_id, 'class_id x y w h'.split()]
         d.to_csv(path / 'labels' / split / f'{image_id}.txt', sep=' ',
