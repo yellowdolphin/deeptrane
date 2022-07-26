6057acac38a498912efbd35924884abe98d970db
yellowdolphin <marius.wanko@gmail.com>
2022-07-26 23:48:56 +0200

    import Path

--- projects/cassava.py
+++ projects/cassava.py
@@ -15,7 +15,6 @@ def init(cfg):
         cfg.image_root = Path(f'/kaggle/input/cassava-jpeg-{size}x{size}/kaggle/train_images_jpeg')
         if cfg.cloud == 'drive':
             cfg.image_root = Path(f'/content/cassava-jpeg-{size}x{size}/kaggle/train_images_jpeg')
-            print("image_root:", cfg.image_root)
     elif cfg.filetype == 'tfrec':
         cfg.image_root = cfg.competition_path / 'train_tfrecords'
 
--- utils/schedulers.py
+++ utils/schedulers.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 import torch
 from torch.optim import lr_scheduler
