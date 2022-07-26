fc37413f92f7156819df3510dfd6d6ae471fca85
yellowdolphin <marius.wanko@gmail.com>
2022-07-26 21:50:10 +0200

    jump to cassava dev state

---
+++ configs/happywhale_tf_aug.py
@@ -0,0 +1,7 @@
+cfg = dict(
+    hflip            = True,
+    random_grayscale = 0.2,
+    random_crop      = 0.4,
+    mean_filter      = 0.5,
+    rotate           = 5,
+)
---
+++ configs/torchvision.py
@@ -0,0 +1,44 @@
+"""
+From torchvision.transforms.functional for compatibility.
+
+`interpolation` type int is deprecated (removed) as of version 0.13 (0.15)
+"""
+
+from enum import Enum
+
+class InterpolationMode(Enum):
+    """Interpolation modes
+    Available interpolation methods are ``nearest``, ``bilinear``, ``bicubic``, ``box``, ``hamming``, and ``lanczos``.
+    """
+
+    NEAREST = "nearest"
+    BILINEAR = "bilinear"
+    BICUBIC = "bicubic"
+    # For PIL compatibility
+    BOX = "box"
+    HAMMING = "hamming"
+    LANCZOS = "lanczos"
+
+
+# TODO: Once torchscript supports Enums with staticmethod
+# this can be put into InterpolationMode as staticmethod
+def _interpolation_modes_from_int(i: int) -> InterpolationMode:
+    inverse_modes_mapping = {
+        0: InterpolationMode.NEAREST,
+        2: InterpolationMode.BILINEAR,
+        3: InterpolationMode.BICUBIC,
+        4: InterpolationMode.BOX,
+        5: InterpolationMode.HAMMING,
+        1: InterpolationMode.LANCZOS,
+    }
+    return inverse_modes_mapping[i]
+
+
+pil_modes_mapping = {
+    InterpolationMode.NEAREST: 0,
+    InterpolationMode.BILINEAR: 2,
+    InterpolationMode.BICUBIC: 3,
+    InterpolationMode.BOX: 4,
+    InterpolationMode.HAMMING: 5,
+    InterpolationMode.LANCZOS: 1,
+}
