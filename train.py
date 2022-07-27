92936664e39427dcdfbe4dbd9c042b7f49b58a48
yellowdolphin <marius.wanko@gmail.com>
2022-07-27 12:05:14 +0200

    fix xla on colab

--- train.py
+++ train.py
@@ -51,12 +51,12 @@ cfg.out_dir = Path(cfg.out_dir)
 # Install torch.xla on kaggle TPU supported nodes
 if 'TPU_NAME' in os.environ:
     cfg.xla = True
-    xla_version = '1.8.1' if (cfg.cloud == 'kaggle') else '1.12.0' # only '1.8.1' works on python3.7
+    xla_version = '1.8.1' #if (cfg.cloud == 'kaggle') else '1.12.0' # only '1.8.1' works on python3.7
 
     # Auto installation
     if cfg.cloud == 'drive': 
         quietly_run(
-            #f'pip install torch=={xla_version}',
+            f'pip install torch=={xla_version}',
             'curl https://raw.githubusercontent.com/pytorch/xla/master/contrib/scripts/env-setup.py -o pytorch-xla-env-setup.py',
             'pip install -U --progress-bar off catalyst',  # for DistributedSamplerWrapper
             debug=True)
