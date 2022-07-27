3a5149e6abd0b5f1cf0d81e22ae505fcc689fa69
yellowdolphin <marius.wanko@gmail.com>
2022-07-27 11:53:13 +0200

    fix xla on colab

--- train.py
+++ train.py
@@ -49,13 +49,14 @@ if cfg.rst_name is not None:
 cfg.out_dir = Path(cfg.out_dir)
 
 # Install torch.xla on kaggle TPU supported nodes
-if (cfg.cloud == 'kaggle') and ('TPU_NAME' in os.environ):
+if 'TPU_NAME' in os.environ:
     cfg.xla = True
-    xla_version = '1.8.1'  # only '1.8.1' works on python3.7
+    xla_version = '1.8.1' if (cfg.cloud == 'kaggle') else '1.12.0+cu113' # only '1.8.1' works on python3.7
 
     # Auto installation
+    if cloud == 'drive': 
+        quietly_run('curl https://raw.githubusercontent.com/pytorch/xla/master/contrib/scripts/env-setup.py -o pytorch-xla-env-setup.py')
     quietly_run(
-        #'curl https://raw.githubusercontent.com/pytorch/xla/master/contrib/scripts/env-setup.py -o pytorch-xla-env-setup.py',
         f'{sys.executable} pytorch-xla-env-setup.py --version {xla_version}',
         #'pip install -U --progress-bar off catalyst',  # for DistributedSamplerWrapper, catalyst 21.8 already installed
         #debug=True
