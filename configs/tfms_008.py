1c20065b5f047b5d7feb13ef850a03bc79020b25
yellowdolphin <marius.wanko@gmail.com>
2022-07-31 23:37:56 +0200

    allow height, width in any albu transform

--- configs/tfms_008.py
+++ configs/tfms_008.py
@@ -9,7 +9,7 @@ cfg = dict(
     train_tfms = alb.Compose([
         alb.HorizontalFlip(p=0.5),
         alb.ColorJitter(p=1.0, brightness=0.1, contrast=(0.8, 1.2), saturation=(0.7, 1.0), hue=0.01),
-        alb.RandomResizedCrop(**size, scale=[0.6, 1], ratio=(0.6, 1.67), interpolation=1, p=0.75),
+        alb.RandomResizedCrop(**size, scale=[0.6, 1], ratio=(0.6, 1.67), interpolation=1),
         alb.Rotate(p=0.5, limit=5, **defaults),
         alb.ColorJitter(p=1.0, brightness=0, contrast=0, saturation=(0, 0), hue=0),
         alb.MedianBlur(blur_limit=(5, 5), p=0.25),
