cb8be3266df435050b07f3b9f166b93c5e1302fd
yellowdolphin <marius.wanko@gmail.com>
2022-07-31 23:28:08 +0200

    tfms_008: like happywhale_tf

--- configs/tfms_008.py
+++ configs/tfms_008.py
@@ -11,7 +11,7 @@ cfg = dict(
         alb.ColorJitter(p=1.0, brightness=0.1, contrast=(0.8, 1.2), saturation=(0.7, 1.0), hue=0.01),
         alb.RandomResizedCrop(**size, scale=[0.6, 1], ratio=(0.6, 1.67), interpolation=1, p=0.75),
         alb.Rotate(p=0.5, limit=5, **defaults),
-        alb.RandomGreyScale(p=0.1),
+        alb.ColorJitter(p=1.0, brightness=0, contrast=0, saturation=(0, 0), hue=0),
         alb.MedianBlur(blur_limit=(5, 5), p=0.25),
         ToTensor(),
     ]),
