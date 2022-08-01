72a870ed102c679d24002a844736faccd6c87eba
yellowdolphin <marius.wanko@gmail.com>
2022-08-01 13:17:19 +0200

    add happywhale_tf

--- projects/happywhale.py
+++ projects/happywhale.py
@@ -11,6 +11,23 @@ from torch import nn
 
 use_tfds = False
 
+# TFRecords
+private_datasets = {}
+crop_methods = {
+    'happywhale-cropped-dataset-yolov5-ds': 'detic_box',
+    'happywhale-tfrecords-unsubmerged': 'unsubmerged', 
+    'happywhale-wds-unsubmerged': None,
+    }
+gcs_paths = {
+    'happywhale-tfrecords-bb': 'gs://kds-78e0d66e23ba96609ed1a523cf56ec15ec84f29f27b87552a60f16f6',
+    'happywhale-tfrecords-backfin': 'gs://kds-13f1a788145b3009a82fa0fef6e890470e153bb00dd2488e5191173b',
+    'backfintfrecords': 'gs://kds-c953863165740d37e881cc034deeb733e92fa4e567c5d920ca2dd678',
+    'happywhale-tfrecords-private2': 'gs://kds-9bd7ea7f4535a67c5ae349b0c8cec3f223d9bb4e80e77283e89eda88',
+    'happywhale-tfrecords-unsubmerged': 'gs://kds-7da70e248765c9851de2db0257775f23164077d70fdf6fcfd74b9dd1',
+    'happywhale-wds-unsubmerged': 'gs://kds-651ab29172b09817da278473d6c028824ff60078e89b80ccc8fbf62a',
+    'happywhale-cropped-dataset-yolov5-ds': 'gs://kds-51b907bdd20683124ee8a9ad48edcfde04041f8a08c4324d0c0c6900',
+    }
+
 
 def init(cfg):
     "Update cfg with project- or context-specific logic."
@@ -45,23 +62,6 @@ def init(cfg):
             cfg.filetype = 'jpg'
         cfg.meta_csv = cfg.competition_path / 'train.csv'
         cfg.dims_csv = Path('/kaggle/input/happywhale-2022-image-dims/dims.csv')
-
-    # TFRecords
-    private_datasets = {}
-    crop_methods = {
-        'happywhale-cropped-dataset-yolov5-ds': 'detic_box',
-        'happywhale-tfrecords-unsubmerged': 'unsubmerged', 
-        'happywhale-wds-unsubmerged': None,
-        }
-    gcs_paths = {
-        'happywhale-tfrecords-bb': 'gs://kds-78e0d66e23ba96609ed1a523cf56ec15ec84f29f27b87552a60f16f6',
-        'happywhale-tfrecords-backfin': 'gs://kds-13f1a788145b3009a82fa0fef6e890470e153bb00dd2488e5191173b',
-        'backfintfrecords': 'gs://kds-c953863165740d37e881cc034deeb733e92fa4e567c5d920ca2dd678',
-        'happywhale-tfrecords-private2': 'gs://kds-9bd7ea7f4535a67c5ae349b0c8cec3f223d9bb4e80e77283e89eda88',
-        'happywhale-tfrecords-unsubmerged': 'gs://kds-7da70e248765c9851de2db0257775f23164077d70fdf6fcfd74b9dd1',
-        'happywhale-wds-unsubmerged': 'gs://kds-651ab29172b09817da278473d6c028824ff60078e89b80ccc8fbf62a',
-        'happywhale-cropped-dataset-yolov5-ds': 'gs://kds-51b907bdd20683124ee8a9ad48edcfde04041f8a08c4324d0c0c6900',
-        }
 
     if cfg.dataset:
         cfg.n_classes = 15587
