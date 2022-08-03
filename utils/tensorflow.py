5750cb8a9a557057c23ae0339a4304f21f71774d
yellowdolphin <marius.wanko@gmail.com>
2022-08-03 14:50:13 +0200

    debug tf lrfn tracing

--- utils/tensorflow.py
+++ utils/tensorflow.py
@@ -43,16 +43,18 @@ def get_lr_callback(cfg, decay='cos', steps_per_epoch=1, plot=False):
 
         if epoch < lr_ramp_ep:
             lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start
-            
-        elif epoch < lr_ramp_ep + lr_sus_ep:
-            lr = lr_max
-            
-        elif decay == 'exp':
-            lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
-
         else:
-            x = (epoch - lr_ramp_ep - lr_sus_ep) / (n_epochs - lr_ramp_ep - lr_sus_ep)
-            lr = (lr_min + lr_max) / 2 + (lr_max - lr_min) / 2 * tf.math.cos(pi * x)
+            lr = lr_max
+
+        #elif epoch < lr_ramp_ep + lr_sus_ep:
+        #    lr = lr_max
+
+        #elif decay == 'exp':
+        #    lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
+
+        #else:
+        #    x = (epoch - lr_ramp_ep - lr_sus_ep) / (n_epochs - lr_ramp_ep - lr_sus_ep)
+        #    lr = (lr_min + lr_max) / 2 + (lr_max - lr_min) / 2 * tf.math.cos(pi * x)
             
         return lr
         
