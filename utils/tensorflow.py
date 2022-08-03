db8b816df03b072c6508b4057e7401fe0698a266
yellowdolphin <marius.wanko@gmail.com>
2022-08-03 18:34:16 +0200

    debug tf lrfn tracing

--- utils/tensorflow.py
+++ utils/tensorflow.py
@@ -33,47 +33,44 @@ def get_lr_callback(cfg, decay='cos', steps_per_epoch=1, plot=False):
     rst_epoch  = tf.constant(cfg.rst_epoch, dtype=tf.float32)
     pi         = tf.constant(math.pi, dtype=tf.float32)
 
-    @tf.function
+    #@tf.function
     def lrfn(iterations):
-        return tf.constant(3e-4, dtype=tf.float32)
-
-    """
         # iterations is tensor with dtype=tf.float32
         #tf.print("lrfn called with", float(iterations), f"({iterations.dtype if hasattr(iterations, 'dtype') else type(iterations)})")
         # lrfn will be called at epoch_start.
         # If passed inside LRSchedule to optimizer, it may be called with optimizer.iterations, once per optimizer step.
+
+        # WARNING:tensorflow:5 out of the last 5 calls to <function get_lr_callback.<locals>.lrfn at 0x7f9e045e43b0> triggered 
+        # tf.function retracing. Tracing is expensive and the excessive number of tracings could be due to 
+        # (1) creating @tf.function repeatedly in a loop, [NO]
+        # (2) passing tensors with different shapes, [NO]
+        # (3) passing Python objects instead of tensors. [NO]
+        # For (1), please define your @tf.function outside of the loop. For (2), @tf.function has experimental_relax_shapes=True option that relaxes argument shapes that can avoid unnecessary retracing. For (3), please refer to https://www.tensorflow.org/guide/function#controlling_retracing and https://www.tensorflow.org/api_docs/python/tf/function for  more details.
+        # - tf2.4.1@kaggle no warn w ramp/lr_max (no elif)
+        # - tf2.4.1@kaggle no warn w ramp/cos (no elif)
+        # - tf2.8.2@colab warns in ep 4, 5 w ramp/lr_sus_ep/cos (1 elif)
+        # - tf2.8.2@colab warns in ep 4, 5 w ramp/cos (no elif)
+        # - tf2.4.1@kaggle no warn w ramp/lr_sus_ep/cos (1 elif)
+        # - tf2.8.2@colab warns in ep 4, w return max_lr (max_lr is tf.constant in closure)
+        # - tf2.8.2@colab warns w return tf.constant(3e-4, dtype=tf.float32) (no closure) still running......................
+
         epoch = iterations / steps_per_epoch + rst_epoch
 
         if epoch < lr_ramp_ep:
-            lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start  # tracing Warning in ep 4, 5
-# WARNING:tensorflow:5 out of the last 5 calls to <function get_lr_callback.<locals>.lrfn at 0x7f9e045e43b0> triggered 
-# tf.function retracing. Tracing is expensive and the excessive number of tracings could be due to 
-# (1) creating @tf.function repeatedly in a loop, [NO]
-# (2) passing tensors with different shapes, [NO]
-# (3) passing Python objects instead of tensors. [NO]
-# For (1), please define your @tf.function outside of the loop. For (2), @tf.function has experimental_relax_shapes=True option that relaxes argument shapes that can avoid unnecessary retracing. For (3), please refer to https://www.tensorflow.org/guide/function#controlling_retracing and https://www.tensorflow.org/api_docs/python/tf/function for  more details.
-# - tf2.4.1@kaggle no warn w ramp/lr_max (no elif)
-# - tf2.4.1@kaggle no warn w ramp/cos (no elif)
-# - tf2.8.2@colab warns in ep 4, 5 w ramp/lr_sus_ep/cos (1 elif)
-# - tf2.8.2@colab warns in ep 4, 5 w ramp/cos (no elif)
-# - tf2.4.1@kaggle no warn w ramp/lr_sus_ep/cos (1 elif)
-# - tf2.8.2@colab warns in ep 4, 5 w return max_lr (max_lr is tf.constant in closure)
-# - tf2.8.2@colab  w return tf.constant(3e-4, dtype=tf.float32) (no closure)
-
+            lr = (lr_max - lr_start) / lr_ramp_ep * epoch + lr_start
 
         elif epoch < lr_ramp_ep + lr_sus_ep:
             lr = lr_max
 
-        #elif decay == 'exp':
-        #    lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
+        elif decay == 'exp':
+            lr = (lr_max - lr_min) * lr_decay**(epoch - lr_ramp_ep - lr_sus_ep) + lr_min
 
         else:
             x = (epoch - lr_ramp_ep - lr_sus_ep) / (n_epochs - lr_ramp_ep - lr_sus_ep)
             lr = (lr_min + lr_max) / 2 + (lr_max - lr_min) / 2 * tf.math.cos(pi * x)
             
         return lr
-    """
-
+        
     if plot:
         n_ep = cfg.epochs - cfg.rst_epoch if (cfg.rst_path and cfg.rst_name) else cfg.epochs
         epochs = tf.range(n_ep, dtype=tf.float32)
