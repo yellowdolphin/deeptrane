e08df8d002198bd1ad34214f2f6f6a5395bc541d
yellowdolphin <marius.wanko@gmail.com>
2022-08-08 23:41:03 +0200

    debug xla/torchmetrics acc/mAP/top5 metrics

--- xla_train.py
+++ xla_train.py
@@ -409,7 +409,8 @@ def _mp_fn(rank, cfg, metadata, wrapped_model, serial_executor, xm, use_fold):
     tmf_top5 = partial(tmf.accuracy, average='micro', top_k=3)
     tmf_top5.__name__ = 'tmf.top5'
     tmf_top5.needs_scores = True
-    tmf_map = partial(tmf.average_precision, num_classes=cfg.n_classes, average='macro')
+    #tmf_map = partial(tmf.average_precision, num_classes=cfg.n_classes, average='macro')  # average needs newer version
+    tmf_map = partial(tmf.average_precision, num_classes=cfg.n_classes)
     tmf_map.__name__ = 'tmf.map'
     tmf_map.needs_scores = True
     #tmf_macro_top5 = partial(tmf.accuracy, average='macro', top_k=5)
