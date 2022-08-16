c01bda544207a366c47bcfc4f1c6fdbb059aa2da
yellowdolphin <marius.wanko@gmail.com>
2022-08-16 13:35:43 +0200

    migrate to torchmetrics

--- projects/cassava.py
+++ projects/cassava.py
@@ -46,6 +46,7 @@ def add_image_id(df, cfg):
     df['image_id'] = df.image_id.str.split('.').str[0]
 
     # DEBUG: truncate data to multiple of TPU global_batch_size * num_folds
+    print("DEBUG: truncating train+valid data to full batches!")
     n_replicas = cfg.n_replicas or 1
     new_len = len(df) - len(df) % (cfg.num_folds * cfg.bs * n_replicas)
     df = df.iloc[:new_len].copy()
