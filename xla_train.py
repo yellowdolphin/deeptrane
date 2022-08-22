35adf42a5c833a99beef5e17937632dd163b8c14
yellowdolphin <marius.wanko@gmail.com>
2022-08-23 00:32:37 +0200

    cassava tests

--- xla_train.py
+++ xla_train.py
@@ -334,6 +334,7 @@ def valid_fn(model, cfg, xm, epoch, dataloader, criterion, device, metrics=None)
         #old_avg_metrics = []
         avg_metrics = metrics.compute()
         if True:
+            xm.master_print("DEBUG0: top_k(acc, top5):", metrics['acc'].top_k, metrics['top5'].top_k)
             xm.master_print("DEBUG1:", avg_metrics['acc'], avg_metrics['top5'])
             xm.master_print("DEBUG2:", metrics['acc'].compute(), metrics['top5'].compute())
         avg_metrics = {k: v.item() if v.ndim == 0 else v.tolist() for k, v in avg_metrics.items()}
