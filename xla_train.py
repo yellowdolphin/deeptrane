9ac9f72dedaaef41f10b8fbf6684604c0f267c21
yellowdolphin <marius.wanko@gmail.com>
2023-02-28 18:10:51 +0100

    recover changes

--- xla_train.py
+++ xla_train.py
@@ -154,7 +154,7 @@ def train_fn(model, cfg, xm, epoch, dataloader, criterion, seg_crit, optimizer, 
             height_width = ((0.5 + 0.5 * torch.rand(2)) * size).to(torch.int)
             top, left = ((size - height_width) * torch.rand(2)).to(torch.int)
             height, width = height_width
-            #inputs = TF.crop(inputs, top, left, height, width)
+            inputs = TF.crop(inputs, top, left, height, width)
             inputs = TF.resize(inputs, cfg.size)
             if torch.rand(1) > 0.5:
                 inputs = TF.hflip(inputs)
