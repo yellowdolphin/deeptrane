fix normalization in Curve.pdf()

projects/autolevels.py
@@ -193,7 +193,10 @@ class Curve():
         (alpha, beta, blackpoint), x = self.prepare_broadcast(params, x)
         x = np.clip(x, 1e-3, None) / 255 * (1 - self.eps)
         y = np.power(x, alpha - 1) * np.power(1 - x, beta - 1)
-        return blackpoint + (255 - blackpoint) * y / y.max()
+        # normalization depends on alpha, beta and must be calculated image/channel-wise
+        hw_axes = [i for i, (a, b) in enumerate(zip(alpha.shape, x.shape)) if b > a]
+        y_norm = y.max(axis=hw_axes, keepdims=True)
+        return blackpoint + (255 - blackpoint) * y / y_norm
 
     def transformed_parameters(self):
         alpha = 1 + np.exp(self.a)