b5ffed1078042c6226813e5483636fe401b08f9a
yellowdolphin <marius.wanko@gmail.com>
2024-06-17 19:03:43 +0200

    add cutout augmentation for TF

--- configs/autolevels_aug_tf.py
+++ configs/autolevels_aug_tf.py
@@ -10,4 +10,6 @@ cfg = dict(
     contrast         = None,
     brightness       = None,
     noise_level      = 0.0,  # random normal noise after resize
+    cutout_max       = 0.2,  # max height/length fraction of blotted area
+    cutout_color     = 'black_or_white',  # color value, 0...1, or one of "black|white|black_or_white|random|noise"
 )
