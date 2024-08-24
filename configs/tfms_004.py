7dbcc5918f27f720c2a15678b994c66c33e6bac8
yellowdolphin <marius.wanko@gmail.com>
2024-08-24 12:02:50 +0200

    alb.ShiftScaleRotate new default value

--- configs/tfms_004.py
+++ configs/tfms_004.py
@@ -2,12 +2,13 @@ import albumentations as alb
 from albumentations.pytorch import ToTensorV2 as ToTensor
 
 size = dict(height=384, width=384)
-defaults = dict(border_mode=4, value=None, interpolation=1)
+defaults = dict(mode=4, cval=0, interpolation=1)
 
 cfg = dict(
     train_tfms = alb.Compose([
         alb.RandomResizedCrop(**size, scale=[0.8, 1], ratio=(0.75, 1.333), interpolation=1),
-        alb.ShiftScaleRotate(p=0.75, shift_limit=0.06, scale_limit=0.25, rotate_limit=0, **defaults),
+        #alb.ShiftScaleRotate(p=0.75, shift_limit=0.06, scale_limit=0.25, rotate_limit=0, **defaults),  # deprecated
+        alb.Affine(p=0.75, scale=(0.75, 1.25), keep_ratio=False, translate_percent=(-0.06, 0.06), rotate=(0, 0), shear=(0, 0), **defaults),
         alb.HorizontalFlip(p=0.5),
         alb.RandomBrightnessContrast(p=0.5, brightness_limit=(-0.05, 0.05), contrast_limit=(-0.05, 0.05)),
         ToTensor(),
